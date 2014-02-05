f237bc010eb33a1f61c7f4567f9ac26dc506bdff
Sagie Maoz <sagie@google.com>
2014-02-05 12:26:12 +0200

    New lines_to_br method
    Converts newlines to HTML <br>s, safely

--- lib/sxle/helpers/html_helper.rb
+++ lib/sxle/helpers/html_helper.rb
@@ -2,8 +2,17 @@ module Sxle
   module Helpers
     module HtmlHelper
       def lines_to_paragraphs(text)
+        convert_lines(text, nil) { |line| content_tag('p', line) }
+      end
+
+      def lines_to_br(text)
+        convert_lines(text, tag(:br)) { |line| CGI::escape_html(line) }
+      end
+
+      private
+      def convert_lines(text, join_with, &block)
         return '' if text.nil? or text =~ /^ *$/
-        text.split("\n").map { |line| content_tag('p', line) }.join().html_safe
+        text.split("\n").map { |line| yield(line) }.join(join_with).html_safe
       end
     end
   end
